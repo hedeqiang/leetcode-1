f6470b19ba54a7d99a61ba5970d8e88b338c5bbe
Kada Liao <kadaliao@gmail.com>
2019-04-03 11:32:37 +0800

    update 82

--- 82.remove-duplicates-from-sorted-list-ii.py
+++ 82.remove-duplicates-from-sorted-list-ii.py
@@ -31,7 +31,7 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+class Solution0(object):
     def deleteDuplicates(self, head):
         """
         :type head: ListNode
@@ -44,21 +44,42 @@ class Solution(object):
         dummy_head.next = head
         pre, slow, fast = dummy_head, head, head.next
 
+        # 通过flag来标记是否在处理相等的情况
         flag = False
         while fast:
             if fast.val == slow.val:
                 flag = True
-                fast = fast.next
             else:
                 if flag:
+                    # 相等结束，将相等部分全部跳过
+                    # 通过pre.next重新定位
                     pre.next = fast
                     flag = False
                 else:
+                    # 遇到不相等情况，前节点指针后移
                     pre = slow
+                # 遇到不相等情况，慢指针指向快指针
                 slow = fast
-                fast = fast.next
+            # 无论哪种情况，快指针往后移
+            fast = fast.next
         if flag:
             pre.next = None
 
         return dummy_head.next
 
+
+class Solution:
+    def deleteDuplicates(self, head):
+        dummy_head = pre = ListNode(None)
+        dummy_head.next = cur = head
+        while cur and cur.next:
+            # 遇到相等的，就把所有相等的都走完
+            if cur.val == cur.next.val:
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                cur = cur.next
+                pre.next = cur
+            else:
+                pre = pre.next
+                cur = cur.next
+        return dummy_head.next
