316715ef59fe8c38ec2d7c6a3f623273bcac2573
Kada Liao <kadaliao@gmail.com>
2019-12-27 20:43:53 +0800

    update 206

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,15 +12,15 @@
 # Testcase Example:  '[1,2,3,4,5]'
 #
 # 反转一个单链表。
-# 
+#
 # 示例:
-# 
+#
 # 输入: 1->2->3->4->5->NULL
 # 输出: 5->4->3->2->1->NULL
-# 
+#
 # 进阶:
 # 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
-# 
+#
 #
 # Definition for singly-linked list.
 # class ListNode:
@@ -28,14 +28,15 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
-    def reverseList0(self, head: ListNode) -> ListNode:
+    def reverseListRecursion(self, head: ListNode) -> ListNode:
         '''递归'''
 
         if not head or not head.next:
             return head
 
-        new_head = self.reverseList(head.next)
+        new_head = self.reverseListRecursion(head.next)
         head.next.next = head
         head.next = None
 
@@ -57,4 +58,3 @@ class Solution:
             cur.next, cur, pre = pre, cur.next, cur
 
         return pre
-
