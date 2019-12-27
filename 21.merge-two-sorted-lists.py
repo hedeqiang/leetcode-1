f4a3c3eff88658002ee2be5828bdc66fca32d189
Kada Liao <kadaliao@gmail.com>
2019-12-27 21:20:14 +0800

    update 21

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -11,43 +11,54 @@
 # Total Submissions: 104.4K
 # Testcase Example:  '[1,2,4]\n[1,3,4]'
 #
-# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
-# 
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+#
 # 示例：
-# 
+#
 # 输入：1->2->4, 1->3->4
 # 输出：1->1->2->3->4->4
-# 
-# 
+#
+#
 #
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        head1, head2, dummy_head= l1, l2, ListNode(None)
+    def mergeTwoLists0(self, l1, l2):
+        head1, head2, dummy_head = l1, l2, ListNode(None)
         cur = dummy_head
-        while head1 or head2:
-            if not head1:
-                cur.next = head2
-                break
-            if not head2:
-                cur.next = head1
-                break
-            if head1.val < head2.val:
+        while head1 and head2:
+            if head1.val <= head2.val:
                 cur.next = head1
                 head1 = head1.next
             else:
                 cur.next = head2
                 head2 = head2.next
             cur = cur.next
+        cur.next = head1 or head2
         return dummy_head.next
+
+    def mergeTwoLists1(self, l1, l2):
+        if l1 and l2:
+            if l1.val < l2.val:
+                l1.next = self.mergeTwoLists1(l1.next, l2)
+                return l1
+            else:
+                l2.next = self.mergeTwoLists1(l1, l2.next)
+                return l2
+        return l1 or l2
+
+    def mergeTwoLists(self, l1, l2):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+
 
