3a8efce3fd5c6fc22d74ea1cf185156d919c629f
Kada Liao <kadaliao@gmail.com>
2019-12-30 10:21:51 +0800

    update 61

--- 61.rotate-list.py
+++ 61.rotate-list.py
@@ -12,18 +12,18 @@
 # Testcase Example:  '[1,2,3,4,5]\n2'
 #
 # 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
-# 
+#
 # 示例 1:
-# 
+#
 # 输入: 1->2->3->4->5->NULL, k = 2
 # 输出: 4->5->1->2->3->NULL
 # 解释:
 # 向右旋转 1 步: 5->1->2->3->4->NULL
 # 向右旋转 2 步: 4->5->1->2->3->NULL
-# 
-# 
+#
+#
 # 示例 2:
-# 
+#
 # 输入: 0->1->2->NULL, k = 4
 # 输出: 2->0->1->NULL
 # 解释:
@@ -31,13 +31,16 @@
 # 向右旋转 2 步: 1->2->0->NULL
 # 向右旋转 3 步: 0->1->2->NULL
 # 向右旋转 4 步: 2->0->1->NULL
-# 
+#
 #
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     def rotateRight(self, head, k):
@@ -51,7 +54,7 @@ class Solution(object):
 
         dummy = ListNode(None)
         l = r = dummy.next = head
-       
+
         # 先找到前后指针
         count = 0
         while count < k and r:
@@ -74,9 +77,9 @@ class Solution(object):
 
         return dummy.next
 
+
 class Solution0(object):
     """k的方向错了"""
-
     def rotateRight(self, head, k):
         """
         :type head: ListNode
@@ -85,7 +88,6 @@ class Solution0(object):
         """
         l = dummy = ListNode(None)
         r = dummy.next = head
-       
 
         count = 0
         while count < k and r:
@@ -108,5 +110,3 @@ class Solution0(object):
         r.next = head
 
         return dummy.next
-
-
