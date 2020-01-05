ea05b48288690f6c85e6444f71b3ec27aae52e26
Kada Liao <kadaliao@gmail.com>
2020-01-05 17:35:13 +0800

    update 142

--- 142.linked-list-cycle-ii.py
+++ 142.linked-list-cycle-ii.py
@@ -12,45 +12,45 @@
 # Testcase Example:  '[3,2,0,-4]\n1'
 #
 # 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
-# 
+#
 # 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
-# 
+#
 # 说明：不允许修改给定的链表。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
+#
 # 输入：head = [3,2,0,-4], pos = 1
 # 输出：tail connects to node index 1
 # 解释：链表中有一个环，其尾部连接到第二个节点。
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 示例 2：
-# 
+#
 # 输入：head = [1,2], pos = 0
 # 输出：tail connects to node index 0
 # 解释：链表中有一个环，其尾部连接到第一个节点。
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 示例 3：
-# 
+#
 # 输入：head = [1], pos = -1
 # 输出：no cycle
 # 解释：链表中没有环。
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
 # 进阶：
 # 你是否可以不用额外空间解决此题？
-# 
+#
 #
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -58,7 +58,8 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+
+class Solution0(object):
     def detectCycle(self, head):
         """
         :type head: ListNode
@@ -75,7 +76,7 @@ class Solution(object):
                 # slow_steps = m + k; 其中m是起始点head到环起始点距离，k是环起始点到相遇点距离;
 
                 # fast走的步长为slow两倍
-                # fast_steps = slow_steps * 2 = slow_steps + N * l, 其中N=1,2,3,4,5.... 
+                # fast_steps = slow_steps * 2 = slow_steps + N * l, 其中N=1,2,3,4,5....
                 # 所以 slow_steps = N*l, 即m + k = N * l
                 # p从头走m+k步，等于q绕N圈；p从头走m步，等于q绕N圈往回退k步，正好位于环起始点，pq会相遇
                 slow = head
