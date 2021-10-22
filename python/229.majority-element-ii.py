df883d18f4a2d6e13a2bf361f3e763df331b5b18
Kada Liao <kadaliao@gmail.com>
2021-10-22 21:56:08 +0800

    update 229

--- python/229.majority-element-ii.py
+++ python/229.majority-element-ii.py
@@ -49,10 +49,46 @@
 #
 # 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1)的算法解决此问题。
 
-from typing import List
-from collections import Counter
+# from collections import Counter
+# from typing import List
+
 
 class Solution:
+    # def majorityElement0(self, nums: List[int]) -> List[int]:
+    #     counter = Counter(nums)
+    #     return [k for k, v in counter.items() if v > len(nums) / 3]
+
     def majorityElement(self, nums: List[int]) -> List[int]:
-        counter = Counter(nums)
-        return [k for k, v in counter.items() if v > len(nums)/3]
+        v1, c1 = None, 0
+        v2, c2 = None, 0
+
+        # 三三消除
+        for num in nums:
+            if c1 and num == v1:
+                c1 += 1
+            elif c2 and num == v2:
+                c2 += 1
+            elif c1 == 0:
+                c1 += 1
+                v1 = num
+            elif c2 == 0:
+                c2 += 1
+                v2 = num
+            else:
+                c1 -= 1
+                c2 -= 1
+
+        # 统计剩余的两个数
+        cnt1, cnt2 = 0, 0
+        for num in nums:
+            if c1 > 0 and num == v1:
+                cnt1 += 1
+            elif c2 > 0 and num == v2:
+                cnt2 += 1
+
+        ret, one_thrid = [], len(nums) / 3
+        if c1 > 0 and cnt1 > one_thrid:
+            ret.append(v1)
+        if c2 > 0 and cnt2 > one_thrid:
+            ret.append(v2)
+        return ret
